0bc4490425d121afca518031aace4469916b98f6
wilsonatb <wilsonatb@gmail.com>
2025-10-03 09:01:33 -0400

    feat: init

---
+++ arrays.php
@@ -0,0 +1,60 @@
+<?php 
+
+/* $books = [
+    "harry potter",
+    "el principito",
+    "el hobbit"
+];
+
+foreach($books as $key => $value) {
+    echo "<li>$key: $value</li>";
+}
+
+echo $books[2]; */
+
+ /* $books = [
+    [
+        "name" => "Cien años de soledad",
+        "author" => "Gabriel García Márquez",
+        "purchaseUrl" => "https://example.com/cien-anos-soledad"
+    ],
+    [
+        "name" => "La sombra del viento",
+        "author" => "Carlos Ruiz Zafón",
+        "purchaseUrl" => "https://example.com/sombra-viento"
+    ],
+    [
+        "name" => "Ficciones",
+        "author" => "Jorge Luis Borges",
+        "purchaseUrl" => "https://example.com/ficciones"
+    ]
+];
+
+echo $books[0]["name"];
+
+?>
+*/
+// crear un array asociativo que contenga pais, capital y poblacion
+
+$paises = [];
+
+/* array_push($paises, [
+    "pais" => "España",
+    "capital" => "Madrid",
+    "poblacion" => 47351567
+]);
+
+array_push($paises, [
+    "pais" => "Francia",
+    "capital" => "París",
+    "poblacion" => 65273511
+]);
+
+array_push($paises, [
+    "pais" => "Italia",
+    "capital" => "Roma",
+    "poblacion" => 59554023
+]); */
+
+
+var_dump($paises);
